1cbf58b3b4ae054651456ec9b95105dcfd91e172
gonzobard777 <gonzo.bard@dwfe.ru>
2023-07-08 10:03:43 +0300

    .

---
+++ ConsoleApp/Database/Sort2Ext.cs
@@ -0,0 +1,37 @@
+using System.Linq.Expressions;
+
+namespace ConsoleApp.Database;
+
+public static class Sort2Ext
+{
+    public static IQueryable<T> AddSorting2<T>(this IQueryable<T> query, string propOrFieldName, bool desc)
+    {
+        var param = Expression.Parameter(typeof(T));
+        var prop = Expression.PropertyOrField(param, propOrFieldName);
+        var sortLambda = Expression.Lambda(prop, param);
+
+        Expression<Func<IOrderedQueryable<T>>> sortMethod;
+        bool isFirstSortingCondition = query.Expression.Type != typeof(IOrderedQueryable<T>);
+
+        if (!desc)
+        {
+            sortMethod = isFirstSortingCondition
+                ? () => query.OrderBy<T, object>(k => null)
+                : () => ((IOrderedQueryable<T>)query).ThenBy<T, object>(k => null);
+        }
+        else
+        {
+            sortMethod = isFirstSortingCondition
+                ? () => query.OrderByDescending<T, object>(k => null)
+                : () => ((IOrderedQueryable<T>)query).ThenByDescending<T, object>(k => null);
+        }
+
+        var methodCallExpression = sortMethod.Body as MethodCallExpression;
+        if (methodCallExpression is null)
+            throw new Exception("MethodCallExpression null");
+
+        var method = methodCallExpression.Method.GetGenericMethodDefinition();
+        var genericSortMethod = method.MakeGenericMethod(typeof(T), prop.Type);
+        return (IOrderedQueryable<T>)genericSortMethod.Invoke(query, new object[] { query, sortLambda });
+    }
+}
--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -12,7 +12,8 @@ public class Program
             // dbContext.Database.EnsureDeleted();
             // dbContext.Database.EnsureCreated();
             
-            SortCols(dbContext);
+            // SortCols(dbContext);
+            // SortCols2(dbContext);
         }
     }
 
@@ -22,6 +23,20 @@ public class Program
             .AddSorting("Col1", SortDirection.Descending)
             .AddSorting("Col2", SortDirection.Ascending)
             .AddSorting("Col3", SortDirection.Descending)
+            .ToList();
+
+        foreach (var item in list)
+        {
+            Console.WriteLine($"{item.Col1}\t{item.Col2}\t{item.Col3}");
+        }
+    }
+    
+    public static void SortCols2(AppDbContext dbContext)
+    {
+        var list = dbContext.Set<Cols>()
+            .AddSorting2("Col1", true)
+            .AddSorting2("Col2", false)
+            .AddSorting2("Col3", true)
             .ToList();
 
         foreach (var item in list)
